a59cddeb4c7d74a4ea3d962ad6a6bc97995be1f5
pvlminh3005 <pvlminh3005@gmail.com>
2021-10-15 09:26:56 +0700

    fix UI detail

--- lib/screens/category_detail_screen.dart
+++ lib/screens/category_detail_screen.dart
@@ -1,14 +1,22 @@
+import 'dart:io';
+
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:intl/intl.dart';
-
 import 'package:provider/provider.dart';
+import 'package:trado_app_uit/components/sale_component.dart';
+import '/screens/rating_and_review_screen.dart';
+
+import '/providers/cart_provider.dart';
+import '/widgets/bage.dart';
+import '../widgets/appbar_widget.dart';
 import '../widgets/scroll_hide_widget.dart';
 import '../widgets/bottom_category_detail.dart';
 import '../models/category_model.dart';
 import '../providers/category_provider.dart';
+import 'cart_screen.dart';
 
 class CategoryDetailScreen extends StatefulWidget {
   static String routeName = '/category_detail';
@@ -21,42 +29,47 @@ class CategoryDetailScreen extends StatefulWidget {
 
 class _CategoryDetailScreenState extends State<CategoryDetailScreen> {
   int activeIndex = 0; //page indicator image
-  int countCategory = 1;
+  int quantityCategory = 1;
+  bool isReadMore = false;
   bool isFavorited = false;
   late ScrollController _scrollController;
 
   Widget _buildImagesSlide(Size size, ThemeData theme, CategoryModel category) {
-    return Container(
-      height: size.height * .5,
-      width: size.width,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.only(
-          bottomLeft: Radius.circular(20),
-          bottomRight: Radius.circular(20),
+    return Hero(
+      transitionOnUserGestures: true,
+      tag: '${category.id}',
+      child: Container(
+        height: Platform.isIOS ? size.height * .55 : size.height * .45,
+        width: size.width,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(20),
+            bottomRight: Radius.circular(20),
+          ),
+          color: Colors.transparent,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black54,
+              offset: Offset(2, 2),
+              blurRadius: 5,
+            )
+          ],
         ),
-        color: Colors.transparent,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black54,
-            offset: Offset(2, 2),
-            blurRadius: 5,
-          )
-        ],
-      ),
-      child: CarouselSlider.builder(
-        itemCount: category.imageUrl.length,
-        itemBuilder: (ctx, index, realIndex) {
-          return _buildImageSlide(category: category, index: index);
-        },
-        options: CarouselOptions(
-          onPageChanged: (index, reason) {
-            setState(() {
-              activeIndex = index;
-            });
+        child: CarouselSlider.builder(
+          itemCount: category.imageUrl.length,
+          itemBuilder: (ctx, index, realIndex) {
+            return _buildImageSlide(category: category, index: index);
           },
-          enlargeCenterPage: true,
-          viewportFraction: 1,
-          disableCenter: true,
+          options: CarouselOptions(
+            onPageChanged: (index, reason) {
+              setState(() {
+                activeIndex = index;
+              });
+            },
+            enlargeCenterPage: true,
+            viewportFraction: 1,
+            disableCenter: true,
+          ),
         ),
       ),
     );
@@ -67,8 +80,8 @@ class _CategoryDetailScreenState extends State<CategoryDetailScreen> {
       activeIndex: activeIndex,
       count: image.length,
       effect: ExpandingDotsEffect(
-        dotWidth: 15,
-        dotHeight: 7,
+        dotWidth: 12,
+        dotHeight: 5,
         activeDotColor: theme.primaryColor,
       ),
     );
@@ -80,15 +93,18 @@ class _CategoryDetailScreenState extends State<CategoryDetailScreen> {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Text(
-            category.title,
-            style: theme.textTheme.caption?.merge(
-              TextStyle(
-                fontWeight: FontWeight.w500,
+          Expanded(
+            child: Text(
+              category.title,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              style: theme.textTheme.caption?.merge(
+                TextStyle(
+                  fontWeight: FontWeight.w500,
+                ),
               ),
             ),
           ),
-          Spacer(),
           GestureDetector(
             onTap: () {
               setState(() {
@@ -115,44 +131,74 @@ class _CategoryDetailScreenState extends State<CategoryDetailScreen> {
     );
   }
 
-  Widget _buildInfoPrice(ThemeData theme, CategoryModel category) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  Widget _buildInfoPrice(
+      ThemeData theme, CategoryModel category, double priceDecreaseSale) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(
-          '${NumberFormat.currency(
-            locale: 'id',
-            decimalDigits: 0,
-            symbol: '',
-          ).format(category.price)} đ',
-          style: theme.textTheme.caption?.merge(
-            TextStyle(
-              fontFamily: 'RobotoCondensed',
-              fontWeight: FontWeight.w700,
-            ),
-          ),
-        ),
+        category.priceSale != 0
+            ? Text(
+                '${NumberFormat.currency(
+                  locale: 'id',
+                  decimalDigits: 0,
+                  symbol: '',
+                ).format(category.price)} đ',
+                style: TextStyle(decoration: TextDecoration.lineThrough),
+              )
+            : SizedBox.shrink(),
         Row(
           children: [
-            ButtonCount(
-              icon: Icons.remove,
-              onPressed: () {
-                setState(() {
-                  if (countCategory <= 0) return;
-                  countCategory--;
-                });
-              },
+            Text(
+              '${NumberFormat.currency(
+                locale: 'id',
+                decimalDigits: 0,
+                symbol: '',
+              ).format(priceDecreaseSale)} đ',
+              style: theme.textTheme.caption?.merge(
+                TextStyle(
+                  color: category.priceSale != 0 ? theme.errorColor : null,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: Text('${countCategory.toString().padLeft(2, "0")}',
-                  style: theme.textTheme.button),
-            ),
-            ButtonCount(
-              icon: Icons.add,
-              onPressed: () => setState(() {
-                countCategory++;
-              }),
+            category.priceSale != 0
+                ? SaleComponent(
+                    text: category.priceSale,
+                    textStyle: theme.textTheme.button?.merge(
+                      TextStyle(fontWeight: FontWeight.w500),
+                    ),
+                  )
+                : SizedBox.shrink(),
+            Spacer(),
+            Row(
+              children: [
+                ButtonCount(
+                  icon: Icons.remove,
+                  onPressed: () {
+                    setState(() {
+                      if (quantityCategory <= 1) return;
+                      quantityCategory--;
+                    });
+                  },
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Text(
+                    '${quantityCategory.toString().padLeft(2, "0")}',
+                    style: theme.textTheme.button?.merge(
+                      TextStyle(
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ),
+                ButtonCount(
+                  icon: Icons.add,
+                  onPressed: () => setState(() {
+                    quantityCategory++;
+                  }),
+                ),
+              ],
             ),
           ],
         ),
@@ -160,42 +206,38 @@ class _CategoryDetailScreenState extends State<CategoryDetailScreen> {
     );
   }
 
-  Widget _buildVoteCategory(ThemeData theme, CategoryModel category) {
+  Widget _buildVoteCategory(
+      BuildContext context, ThemeData theme, CategoryModel category) {
     return GestureDetector(
-      onTap: () {
-        print('CLICK');
-      },
+      onTap: () => Navigator.of(context)
+          .pushNamed(RatingAndReviewScreen.routeName, arguments: category),
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: 10),
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Row(
-              children: [
-                Icon(
-                  Icons.star_rounded,
-                  color: category.amountVoteStars == 0
-                      ? Color(0xFF777777)
-                      : theme.highlightColor,
-                  size: 35,
-                ),
-                Text(
-                  '${category.amountVoteStars}',
-                  style: theme.textTheme.headline3,
-                ),
-                SizedBox(width: 10),
-                Text(
-                  category.amoutComment == 0
-                      ? 'Chưa có nhận xét nào'
-                      : '(${category.amoutComment} nhận xét)',
-                  style: theme.textTheme.headline3?.merge(
-                    TextStyle(
-                      color: theme.textSelectionColor,
-                    ),
-                  ),
-                ),
-              ],
+            Icon(
+              Icons.star_rounded,
+              color: category.amountVoteStars == 0
+                  ? theme.textSelectionColor
+                  : theme.highlightColor,
+              size: 30,
             ),
+            Text(
+              '${category.amountVoteStars}',
+              style: theme.textTheme.headline3,
+            ),
+            SizedBox(width: 10),
+            Text(
+              category.amoutComment == 0
+                  ? 'Chưa có nhận xét nào'
+                  : '(${category.amoutComment} nhận xét)',
+              style: theme.textTheme.headline3?.merge(
+                TextStyle(
+                  color: theme.textSelectionColor,
+                ),
+              ),
+            ),
+            Spacer(),
             Row(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
@@ -221,18 +263,57 @@ class _CategoryDetailScreenState extends State<CategoryDetailScreen> {
   }
 
   Widget _buildDescription(ThemeData theme, CategoryModel category) {
-    return Container(
-      child: Text(
-        category.description,
-        style: theme.textTheme.headline3?.merge(
-          TextStyle(
-            color: Colors.grey.shade700,
-            letterSpacing: .5,
+    return Column(
+      children: [
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 7),
+          child: _buildText(theme, category.description),
+        ),
+        GestureDetector(
+          onTap: () {
+            setState(() {
+              isReadMore = !isReadMore;
+            });
+          },
+          child: Padding(
+            padding: EdgeInsets.only(bottom: Platform.isIOS ? 15 : 0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  isReadMore ? 'Thu gọn' : 'Xem thêm',
+                  style: theme.textTheme.headline2
+                      ?.merge(TextStyle(color: theme.textSelectionColor)),
+                ),
+                Icon(
+                  isReadMore
+                      ? Icons.keyboard_arrow_up
+                      : Icons.keyboard_arrow_down,
+                  color: theme.textSelectionColor,
+                  size: 18,
+                ),
+              ],
+            ),
           ),
         ),
-        overflow: TextOverflow.fade,
-        // maxLines: 7,
+      ],
+    );
+  }
+
+  Widget _buildText(ThemeData theme, String text) {
+    final int platform = Platform.isIOS ? 6 : 5;
+    final maxLines = isReadMore ? null : platform;
+    final overflow = isReadMore ? TextOverflow.visible : TextOverflow.fade;
+    return Text(
+      text,
+      style: theme.textTheme.headline3?.merge(
+        TextStyle(
+          color: Colors.grey.shade700,
+          letterSpacing: .5,
+        ),
       ),
+      overflow: overflow,
+      maxLines: maxLines,
     );
   }
 
@@ -256,6 +337,9 @@ class _CategoryDetailScreenState extends State<CategoryDetailScreen> {
     CategoryModel category =
         Provider.of<CategoryProvider>(context, listen: false)
             .findCategoryById(idCategory);
+    double priceDecreaseSale = category.priceSale != 0
+        ? (category.price * (100 - category.priceSale) / 100)
+        : category.price;
 
     Size size = MediaQuery.of(context).size;
     ThemeData theme = Theme.of(context);
@@ -263,12 +347,22 @@ class _CategoryDetailScreenState extends State<CategoryDetailScreen> {
     return Scaffold(
       backgroundColor: theme.backgroundColor,
       extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-        leading: IconButton(
-          onPressed: () => Navigator.of(context).pop(),
-          icon: Icon(Icons.arrow_back_ios),
+      appBar: AppBarWidget(
+        color: Colors.white,
+        childAction: Consumer<CartProvider>(
+          builder: (ctx, cartData, ch) => Badge(
+            child: IconButton(
+              icon: Icon(
+                Icons.shopping_cart,
+                size: 25,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                Navigator.of(context).pushNamed(CartScreen.routeName);
+              },
+            ),
+            value: cartData.cartCount.toString(),
+          ),
         ),
       ),
       body: Column(
@@ -292,8 +386,8 @@ class _CategoryDetailScreenState extends State<CategoryDetailScreen> {
                 child: Column(
                   children: [
                     _buildInfoTitle(theme, category),
-                    _buildInfoPrice(theme, category),
-                    _buildVoteCategory(theme, category),
+                    _buildInfoPrice(theme, category, priceDecreaseSale),
+                    _buildVoteCategory(context, theme, category),
                     _buildDescription(theme, category),
                   ],
                 ),
@@ -306,7 +400,10 @@ class _CategoryDetailScreenState extends State<CategoryDetailScreen> {
           FloatingActionButtonLocation.miniCenterDocked,
       floatingActionButton: ScrollToHideWidget(
         controller: _scrollController,
-        child: BottomCategoryDetail(),
+        child: BottomCategoryDetail(
+          category: category,
+          quantity: quantityCategory,
+        ),
       ),
     );
   }
@@ -324,18 +421,14 @@ class _buildImageSlide extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Hero(
-      transitionOnUserGestures: true,
-      tag: category.imageUrl[index],
-      child: ClipRRect(
-        borderRadius: BorderRadius.only(
-          bottomLeft: Radius.circular(20),
-          bottomRight: Radius.circular(20),
-        ),
-        child: Image(
-          image: NetworkImage(category.imageUrl[index]),
-          fit: BoxFit.cover,
-        ),
+    return ClipRRect(
+      borderRadius: BorderRadius.only(
+        bottomLeft: Radius.circular(20),
+        bottomRight: Radius.circular(20),
+      ),
+      child: Image(
+        image: NetworkImage(category.imageUrl[index]),
+        fit: BoxFit.cover,
       ),
     );
   }
@@ -357,7 +450,7 @@ class ButtonCount extends StatelessWidget {
     return GestureDetector(
       onTap: onPressed,
       child: Container(
-        padding: const EdgeInsets.all(8),
+        padding: const EdgeInsets.all(7),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(15),
           color: theme.accentColor,
